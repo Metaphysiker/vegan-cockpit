277edddac8bf0f87d9a0670c63303a183287005d
Metaphysiker <s.raess@me.com>
2022-08-18 18:25:36 +0200

    problem with gapi.client

--- src/app/google-analytics.service.ts
+++ src/app/google-analytics.service.ts
@@ -15,10 +15,18 @@ export class GoogleAnalyticsService {
 
   getData(): any {
     var data = ["abc", "def"];
+    var url = 'https://reqres.in/api/users';
+
+    //let queryParams = new HttpParams();
+    //queryParams = queryParams.append("page",1);
 
     return this.http.get<any>('https://api.publicapis.org/entries');
 
     //return data;
+  }
+
+  getGoogleData(view_id: any, dateRange: any, relative_url: any): any {
+
   }
 
 }
