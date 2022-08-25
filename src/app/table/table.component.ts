365dc8256b570bcf3f55197bcb0da50c42774396
Metaphysiker <s.raess@me.com>
2022-08-25 18:26:34 +0200

    works

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -15,7 +15,9 @@ export class TableComponent implements OnInit {
     [
       {a: "123", b: "alphabet", c: "s.raess@me.com", d: 123},
       {a: "456", b: "camma", c: "sandro.raess@philosophie.ch", d: 345},
-      {a: "789", b: "beta", c: "sandro@vegan.ch", d: 456}
+      {a: "789", b: "beta", c: "sandro@vegan.ch", d: 456},
+      {a: "0.2", b: "beta", c: "sandro@vegan.ch", d: 0.2},
+      {a: " 50", b: "beta", c: "sandro@vegan.ch", d: 50}
     ]
 
   constructor() { }
@@ -25,9 +27,17 @@ export class TableComponent implements OnInit {
 
   sort(key: any, order: string): void {
 
-    this.rows.sort((a, b) => {
-      return a[key].toString().localeCompare(b[key].toString())
-    });
+    if (isNaN(this.rows[0][key])) {
+
+      this.rows.sort((a, b) => {
+        return a[key].toString().localeCompare(b[key].toString());
+      });
+
+    } else {
+      this.rows.sort((a, b) => {
+        return a[key] - b[key];
+      });
+    }
 
     if(order == 'ASC'){
       this.rows.reverse();
